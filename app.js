7c34dd9b6ee0a83afd95a99a8f805e32ed22a474
Mohamed Razzk <mohamedrazzk@gamil.com>
2022-04-11 17:55:17 +0200

    cors update

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const compression = require('compression'); 
+const cors = require('cors');
+
 
 const postRouter = require('./routes/postRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -19,26 +21,33 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(cors());
+app.options('*', cors());
+
+
+
 // ================================================================================================
 //                                 1) Middlewares
 // ================================================================================================
+
+
+// Serve static files in public folder(ex: home.html)
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Log the information about the request that we did only if we are in development. dev is an argument that specify how the logging will look like
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
 // rate limit funcation
 const limiter = rateLimit({
   max: 100,
   windowMS: 60 * 60 * 1000,
   message: 'To manny request from this ip, please try again later'
 });
-
 app.use('/api/', limiter);
 //set security http requist
 app.use(helmet());
-
 // data sanatization against no sql quiress
 app.use(mongoSanitize());
 // data sanatization against xss
@@ -47,27 +56,13 @@ app.use(xss());
 app.use(hpp());
 // In this middleware the data from the body is added to the request (req.body becomes available)
 app.use(express.json());
-
 //compression 
 app.use(compression())
-
 // Add the current time to the request by defining a property requestTime on the request, toISOString is a date function that converts it to a readable string
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'content-type, authorization');
-  res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  next();
-});
-
-
-// Serve static files in public folder(ex: home.html)
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 
@@ -80,12 +75,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.redirect(301,'http://crefto.studio/');
 });
-
 // Mount postRouter to that route
 app.use('/api/v1/posts', postRouter);
 // Mount userRouter to that route
 app.use('/api/v1/users', userRouter);
-
 //404 
 app.use((req, res)=>{
   res.redirect(301,'http://crefto.studio/');
